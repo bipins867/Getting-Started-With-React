6d0c85a89a7862b7618dddf5cbffc416569fc619
= <=>
2023-09-19 17:48:19 +0530

    Create simple Expense List

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items!</h2>
+      <div>
+        <h3>Food Rs 10</h3>
+      </div>
+      <div>
+        <h3>Petrol Rs 100</h3>
+      </div>
+      <div>
+        <h3>Movies Rs 200</h3>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
