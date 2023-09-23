cf6a2ea55d63019282efdbc8cf30212d2228569e
= <=>
2023-09-23 11:38:41 +0530

    Using filter for the expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -9,23 +9,27 @@ const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = selectedYear => {
+    
     setFilteredYear(selectedYear);
   };
 
+  const yearExpense=props.items.filter(expense=>expense.date.getFullYear()==filteredYear)
+
+  const expenseList=yearExpense.map((expense) => {
+    return (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    );
+  });
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map(expense=>{
-          return (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          );
-        })}
+        {expenseList}
         
         
       </Card>
