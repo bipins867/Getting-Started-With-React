9eebdc15fc93d264435e94dfe843670928825ac8
= <=>
2023-09-23 17:11:11 +0530

    Adding Cancel For options

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
@@ -7,16 +7,28 @@ const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
+  const addExpenseButton = (
+    <button onClick={addExpenseFormHandler}>Add Expense</button>
   );
+
+  const [formContent, setFormContent] = useState(addExpenseButton);
+
+
+  function addExpenseFormHandler(event){
+    setFormContent(<ExpenseForm cancelFormHandler={onCancelFormHandler}  onSaveExpenseData={saveExpenseDataHandler} />);
+  }
+
+  function onCancelFormHandler(){
+    setFormContent(addExpenseButton)
+  }
+  
+  //<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+  return <div className="new-expense">{formContent}</div>;
 };
 
 export default NewExpense;
